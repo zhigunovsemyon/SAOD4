1b345b8b77cf63ff433d889e7ef85110902e02ab
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-09-30 20:58:26 +0300

    Перегнал свой qsort на дин память (всё ещё stack overflow)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>  /*printf*/
 #include <stdlib.h> /*EXIT_SUCCESS*/
 #include <string.h> /*memcpy*/
+#include <stdbool.h> /*bool, true, false*/
 
 // Однобайтовый тип данных
 #define byte char
@@ -13,38 +14,56 @@ void PrintArray(int const *const Arr, size_t const len) {
     putchar('\n');
 }
 
-// void My_quicksort(void *const source, size_t len, size_t sizeofel,
-// int(*compar)(void*,void*)) { 	byte *mem = (byte *)source;//Текущий
-// указатель
-// на память 	byte arr1[len * sizeofel], arr2[len * sizeofel]; // Два плеча
-// массива 	size_t sizeof1arr = 0, sizeof2arr = 0; //Длинны массивов
-//
-//	byte pivot[sizeofel]; //Ключ сортировки
-//	memcpy(pivot, mem, sizeofel);
-//
-//	for (size_t i = 0; i < len; i++){
-//		int comp_res = compar(pivot, mem);
-//		if (comp_res < 0)	{
-//			memcpy(arr1, mem, sizeofel);
-//			sizeof1arr++;
-//		} else {
-//			memcpy(arr2, mem, sizeofel);
-//			sizeof2arr++;
-//		}
-//		mem += sizeofel;
-//	}
-//
-//	if (sizeof1arr > 1){
-//		My_quicksort(arr1, sizeof1arr, sizeofel, compar);
-//	}
-//	if (sizeof2arr > 1){
-//		My_quicksort(arr2, sizeof2arr, sizeofel, compar);
-//	}
-//
-//	memcpy((byte *)source, arr1, sizeof1arr * sizeofel);
-//	memcpy((byte *)source + (sizeof1arr * sizeofel), arr2, sizeof2arr *
-// sizeofel);
-// }
+bool My_quicksort(void *const source, size_t const len, size_t const sizeofel,
+                  int (*compar)(const void *, const void *)) {
+    byte *mem = (byte *)source; // Текущий указатель на память
+
+    byte *arr1 = (byte *)malloc(len * sizeofel); // Левое плечо массива
+    byte *arr2 = (byte *)malloc(len * sizeofel); // Правое плечо массива
+    byte *pivot = (byte *)malloc(sizeofel); // Ключ сортировки
+
+    /* Проверка выделения памяти.Если хоть один раз выделить память не
+        удалось, сортировщик завершает работу */
+    if (!(arr1 && arr2 && pivot)) {
+        free(arr1);
+        free(arr2);
+        free(pivot);
+        return true;
+    }
+
+    size_t sizeof1arr = 0, sizeof2arr = 0; // Длинны массивов
+    memcpy(pivot, mem, sizeofel);
+
+    for (size_t i = 0; i < len; i++) {
+        int comp_res = compar(pivot, mem);
+        if (comp_res < 0) {
+            memcpy(arr1, mem, sizeofel);
+            sizeof1arr++;
+        } else {
+            memcpy(arr2, mem, sizeofel);
+            sizeof2arr++;
+        }
+        mem += sizeofel;
+    }
+
+    if (sizeof1arr > 1) {
+        if(My_quicksort(arr1, sizeof1arr, sizeofel, compar))
+            return true;
+    }
+    if (sizeof2arr > 1) {
+        if(My_quicksort(arr2, sizeof2arr, sizeofel, compar))
+            return true;
+    }
+
+    memcpy((byte *)source, arr1, sizeof1arr * sizeofel);
+    memcpy((byte *)source + (sizeof1arr * sizeofel), arr2,
+           sizeof2arr * sizeofel);
+
+    free(pivot);
+    free(arr1);
+    free(arr2);
+    return false;
+}
 
 // Функция сортировки int чисел по возрастанию
 int AscIntSort(const void *const a, const void *const b) {
@@ -60,23 +79,22 @@ int NoSort(const void *const a, const void *const b) {
 
 int main(void) {
     int const A[] = {
+        /*10, 1, 3, -9, 11, -8, 0, 1, 3, 4, 89, 122, 0, -88, -2, 23, 10, -3, 1,
         10, 1, 3, -9, 11, -8, 0, 1, 3, 4, 89, 122, 0, -88, -2, 23, 10, -3, 1,
         10, 1, 3, -9, 11, -8, 0, 1, 3, 4, 89, 122, 0, -88, -2, 23, 10, -3, 1,
         10, 1, 3, -9, 11, -8, 0, 1, 3, 4, 89, 122, 0, -88, -2, 23, 10, -3, 1,
-        10, 1, 3, -9, 11, -8, 0, 1, 3, 4, 89, 122, 0, -88, -2, 23, 10, -3, 1,
-        10, 1, 3, -9, 11, -8, 0, 1, 3, 4, 89, 122, 0, -88, -2, 23, 10, -3, 1,
-        10, 1, 3, -9, 11, -8, 0, 1, 3, 4, 89, 122, 0, -88, -2, 23, 10, -3, 1
-    };
-    //Размер массива
+        10, 1, 3, -9, 11, -8, 0, 1, 3, 4, 89, 122, 0, -88, -2, 23, 10, -3, 1,*/
+        10, 1, 3, -9, 11, -8, 0, 1, 3, 4, 89, 122, 0, -88, -2, 23, 10, -3, 1};
+    // Размер массива
     size_t const A_len = sizeof(A) / sizeof(int);
 
-    printf("Массив до изменения: ");
+    printf("Массив до изменения:\t");
     PrintArray(A, A_len);
 
     // qsort((void *)A, A_len, sizeof(int), AscIntSort);
-    qsort((void *)A, A_len, sizeof(int), NoSort);
+    My_quicksort((void *)A, A_len, sizeof(int), NoSort);
 
-    printf("Массив после изменения: ");
+    printf("Массив после изменения:\t");
     PrintArray(A, A_len);
 
     return EXIT_SUCCESS;
